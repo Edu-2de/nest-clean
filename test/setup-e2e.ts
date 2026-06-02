acf4bd9a00ef9db362fc42410815626d48368fd7
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-06-02 17:08:47 -0300

    feat(test): add e2e setup with Prisma client and unique database schema handling

---
+++ test/setup-e2e.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from '@/generated/prisma/client'; // Ajuste conforme o seu path
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+
+let prisma: PrismaClient;
+let schemaId: string;
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is required to run e2e tests');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set('schema', schemaId);
+  return url.toString();
+}
+
+beforeAll(async () => {
+  schemaId = randomUUID();
+  const databaseURL = generateUniqueDatabaseURL(schemaId);
+
+  process.env.DATABASE_URL = databaseURL;
+
+  const adapter = new PrismaPg({ connectionString: databaseURL });
+  prisma = new PrismaClient({ adapter });
+
+  execSync('pnpm prisma migrate deploy', {
+    env: {
+      ...process.env,
+      DATABASE_URL: databaseURL,
+    },
+  });
+});
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`);
+
+  await prisma.$disconnect();
+});
