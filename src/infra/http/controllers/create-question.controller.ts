506fe608981ce86082e1e84806b7df05658ed66e
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-06-03 18:25:57 -0300

    feat: refactor PrismaService import paths and add DatabaseModule with PrismaService

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,7 +1,7 @@
 import { CurrentUser } from '@/infra/auth/current-user.decorator'
 import type { TokenPayload } from '@/infra/auth/jwt.strategy'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe'
-import { PrismaService } from '@/infra/prisma/prisma.service'
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 import { randomUUID } from 'crypto'
