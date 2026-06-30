0ef4fe598f2a847263ca0689df628574ff0ef5de
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-06-30 16:17:34 -0300

    refactor: change controller to use correct presenters and mappers

---
+++ src/infra/database/prisma/mappers/prisma-question-details-mapper.ts
@@ -0,0 +1,31 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import {
+  Attachment as PrismaAttachment,
+  Question as PrismaQuestion,
+  User as PrismaUser,
+} from '@/generated/prisma/client'
+import { PrismaAttachmentMapper } from './prisma-attachment-mapper'
+
+type PrismaQuestionDetailsProps = PrismaQuestion & {
+  author: PrismaUser
+  attachments: PrismaAttachment[]
+}
+
+export class PrismaQuestionDetailsMapper {
+  static toDomain(raw: PrismaQuestionDetailsProps): QuestionDetails {
+    return QuestionDetails.create({
+      questionId: new UniqueEntityId(raw.id),
+      authorId: new UniqueEntityId(raw.authorId),
+      author: raw.author.name,
+      title: raw.title,
+      slug: Slug.create(raw.slug),
+      attachments: raw.attachments.map(PrismaAttachmentMapper.toDomain),
+      bestAnswerId: raw.bestAnswerId ? new UniqueEntityId() : null,
+      content: raw.content,
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    })
+  }
+}
