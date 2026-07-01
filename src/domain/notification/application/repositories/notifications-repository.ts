f77a1cbc23724954c25f12e36d89e38b7cdd4dd8
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-07-01 14:32:14 -0300

    feat: implement notification system with database integration and repository pattern

--- src/domain/notification/application/repositories/notifications-repository.ts
+++ src/domain/notification/application/repositories/notifications-repository.ts
@@ -1,7 +1,7 @@
 import type { Notification } from '../../enterprise/entities/notification'
 
-export interface NotificationsRepository {
-  findById(id: String): Promise<Notification | null>
-  create(notification: Notification): Promise<void>
-  save(notification: Notification): Promise<void>
+export abstract class NotificationsRepository {
+  abstract findById(id: String): Promise<Notification | null>
+  abstract create(notification: Notification): Promise<void>
+  abstract save(notification: Notification): Promise<void>
 }
--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -1,10 +1,13 @@
-import { UniqueEntityId } from '@/core/entities/unique-entity-id.js';
+import { UniqueEntityId } from '@/core/entities/unique-entity-id.js'
 import {
   type NotificationProps,
   Notification,
-} from '@/domain/notification/enterprise/entities/notification.js';
+} from '@/domain/notification/enterprise/entities/notification.js'
+import { PrismaNotificationMapper } from '@/infra/database/prisma/mappers/prisma-notification-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
-import { faker } from '@faker-js/faker';
+import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 
 export function makeNotification(
   override: Partial<NotificationProps> = {},
@@ -18,7 +21,23 @@ export function makeNotification(
       ...override,
     },
     id,
-  );
+  )
 
-  return notification;
+  return notification
+}
+@Injectable()
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaNotification(
+    data: Partial<NotificationProps> = {},
+  ): Promise<Notification> {
+    const notification = makeNotification(data)
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification),
+    })
+
+    return notification
+  }
 }
